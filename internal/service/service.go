fc910f650f4deed290aa1078274e5380e8b566ec
OlgaMalyshevaa <147718948+OlgaMalyshevaa@users.noreply.github.com>
2025-05-13 08:16:26 +0300

    Update service.go

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,31 +1,3 @@
-// package service
-
-// import (
-// 	"context"
-// 	"errors"
-// 	"wallet/internal/model"
-// 	"wallet/internal/repository"
-// )
-
-// type WalletService struct {
-// 	repo *repository.WalletRepository
-// }
-
-// func NewWalletService(repo *repository.WalletRepository) *WalletService {
-// 	return &WalletService{repo: repo}
-// }
-
-// func (s *WalletService) PerformTransaction(ctx context.Context, req model.TransactionRequest) error {
-// 	if req.Amount <= 0 {
-// 		return errors.New("amount must be positive")
-// 	}
-// 	return s.repo.InsertTransaction(ctx, req)
-// }
-
-// func (s *WalletService) GetBalance(ctx context.Context, id string) (int64, error) {
-// 	return s.repo.CalculateBalance(ctx, id)
-// }
-
 package service
 
 import (
